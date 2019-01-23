5fff0386ef524b090a1703cf0bb1735590b15a80
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-22 21:52:03 -0700

    Add functionality that changes the color palette divs

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,22 +1,27 @@
+$('.generate-palette-btn').on('click', generateNewPalette)
 
-const generateNewPalette = () => {
+function generateNewPalette() {
   let paletteColors = []
   for (let i = 0; i < 5; i++) {
     const hexCode = generateHexCode()
     paletteColors.push(hexCode)
   }
-  console.log(paletteColors)
+  updatePaletteColors(paletteColors)
 }
 
-const generateHexCode = () => {
+function generateHexCode() {
   const letters = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   let hexCode = ''
   for (let i = 0; i < 6; i++) {
     const randomCharacter = Math.floor(Math.random() * letters.length)
     hexCode = hexCode.concat(letters[randomCharacter])
   }
-  console.log(hexCode)
   return hexCode
 }
 
-$('.generate-palette-btn').on('click', generateNewPalette)
+function updatePaletteColors(paletteColors) {
+  $('.color').each(function(index) {
+    console.log(this)
+    this.style.background = `#${paletteColors[index]}`
+  })
+}
