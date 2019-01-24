f1cf7dd1c31ea71d9dcdc2310fcab8f4ce8930fa
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-24 14:32:12 -0700

    Add function in server to handle adding a project

--- server.js
+++ server.js
@@ -28,6 +28,21 @@ app.get('/api/v1/palettes', (request, response) => {
     })
 })
 
+app.put('/api/v1/project', (request, response) => {
+  const project = request.body;
+
+  for (let requiredParam of ['project']) {
+    if(!project[requiredParam]) {
+      return response.status(422)
+        .send({error: `Expected format to create a new project is: { project: <String> }. You are missing a ${ requiredParam } property.`})
+    }
+  }
+
+  database('projects').insert(project, 'id')
+    .then(project => response.status(201).json({ id: project[0] }))
+    .catch(error => response.status(500).json({ error }))
+})
+
 app.listen(3000, () => {
   console.log('Palette Picker running on localhost:3000')
 })
